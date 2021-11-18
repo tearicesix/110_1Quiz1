9d3139d608f0dc0050b2b2f55332aed23cd3dd5e
冠登 廖 <t8599008@ntut.org.tw>
2021-11-18 15:52:00 +0800

    加入專案檔案。

---
+++ 110_1Quiz1/Ad.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Quiz1 {
+    public partial class Ad : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
